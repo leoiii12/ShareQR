2b1236238dc9e7fec1875b79dd7ec501543e3afa
Leo Choi <choimankin@gmail.com>
2018-03-29 02:02:02 +0800

    1. Added back App.xaml.cs without secrets

---
+++ ShareQR/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using ShareQR.Views;
+using Xamarin.Forms;
+using Device = Xamarin.Forms.Device;
+
+namespace ShareQR
+{
+    public partial class App : Application
+    {
+        public App(AppSetup setup)
+        {
+            InitializeComponent();
+
+            AppContainer.Container = setup.CreateContainer();
+
+            if (Device.RuntimePlatform == Device.iOS)
+                MainPage = new MainPage();
+            else
+                MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            MessagingCenter.Send(this, "OnResume");
+        }
+    }
+}
